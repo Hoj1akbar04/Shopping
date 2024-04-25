17e74359517ada41fbec316f687f3aac1e3391c2
Hojiakbar <hojiakbaribodullayev04@gmail.com>
2024-04-25 20:22:15 +0500

    this is website for exam

--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["178.128.41.226"]
 
 LOGIN_URL = "login"
 
