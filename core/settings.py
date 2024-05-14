51610cc3aeccb887c9958a768ce93f2a91150eff
Hojiakbar <hojiakbaribodullayev04@gmail.com>
2024-05-15 00:10:18 +0500

    add

--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["161.35.161.60"]
+ALLOWED_HOSTS = []
 
 LOGIN_URL = "login"
 
